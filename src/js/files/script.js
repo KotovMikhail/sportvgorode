5a94aa23f53214b24d41351563db456cac8bb8f6
Mikhail <mihail_kotov_86@mail.ru>
2022-03-16 17:20:19 +0300

    Last commit

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,23 @@
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
+
+import $ from "jquery";
+import "Selectric";
+
+
+
+
+
+$(function () {
+    $('select').selectric({
+        arrowButtonMarkup: '<svg class="arrow" width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L7 7L13 1" stroke="#C4C4C4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+   
+    });
+});
+
+$(function () {
+    $('select2').selectric({
+        arrowButtonMarkup: '<svg class="arrow" width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L7 7L13 1" stroke="#C4C4C4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+    });
+});
